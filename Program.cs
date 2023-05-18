d29ccc1d0fe4f656dad8650b2e81fa1a8548b31d
Jason Harris <lord.jtharris@gmail.com>
2023-05-18 10:52:41 -0500

    First iteration. Ask for input, redisplay input.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+void GuessingGame(){
+    //int guessNum = 1;
+    Console.WriteLine("Welcome to the Guessing Game!");
+    Console.WriteLine("You will be guessing my SECRET! number. The range is 1-100, inclusive.");
+    Console.WriteLine("Enter your guess: ");
+    var userGuess = Console.ReadLine();
+    Console.WriteLine($"Your guess was: {userGuess}");
+}
+
+GuessingGame();
