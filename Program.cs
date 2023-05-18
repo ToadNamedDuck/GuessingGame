bdde21b8e221cd66790ea3ccfc0c3d569fe24cb8
Jason Harris <lord.jtharris@gmail.com>
2023-05-18 11:07:57 -0500

    Now you can actually guess a hard-coded number, with a success or failure message.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 
 void GuessingGame(){
-    //int guessNum = 1;
+    //int guessTracker = 1;
+    int secretNumber = 42;
     Console.WriteLine("Welcome to the Guessing Game!");
     Console.WriteLine("You will be guessing my SECRET! number. The range is 1-100, inclusive.");
+    Console.WriteLine("PLEASE: Only enter in a number!");
     Console.WriteLine("Enter your guess: ");
-    var userGuess = Console.ReadLine();
-    Console.WriteLine($"Your guess was: {userGuess}");
+    var userGuess = int.Parse(Console.ReadLine());
+    if(userGuess == secretNumber){
+        Console.WriteLine($"How did you guess my secret number! It was {secretNumber} all along!");
+    }
+    else{
+        Console.WriteLine("Alas, not this time! Sorry!");
+    }
 }
 
 GuessingGame();
