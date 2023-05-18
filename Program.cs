16dd9e9c463a2ad81839f87c1f02434c0c16364f
Jason Harris <lord.jtharris@gmail.com>
2023-05-18 11:44:10 -0500

    Guess 4 times. Success message or fail message. Shows the current guess user is on.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 
-void GuessingGame(){
-    //int guessTracker = 1;
+void GuessingGame()
+{
+    int guessTracker = 0;
     int secretNumber = 42;
+    int trackedGuess;
     Console.WriteLine("Welcome to the Guessing Game!");
     Console.WriteLine("You will be guessing my SECRET! number. The range is 1-100, inclusive.");
     Console.WriteLine("PLEASE: Only enter in a number!");
-    Console.WriteLine("Enter your guess: ");
-    var userGuess = int.Parse(Console.ReadLine());
-    if(userGuess == secretNumber){
-        Console.WriteLine($"How did you guess my secret number! It was {secretNumber} all along!");
-    }
-    else{
-        Console.WriteLine("Alas, not this time! Sorry!");
-    }
+    Console.WriteLine($"Enter your guess ({guessTracker + 1}): ");
+    
+    do//do...while to keep track of the userGuess and compare it to an outside-scope variable, if the input can change.
+    {
+        int userGuess = int.Parse(Console.ReadLine());
+        trackedGuess = userGuess;
+
+        if (userGuess != secretNumber && guessTracker != 3)
+        {
+            Console.WriteLine("Alas, not this time, but maybe next time!");
+            if (guessTracker < 3)//prevents these lines from going through after the final input session.
+            {
+                guessTracker++;
+                Console.WriteLine($"Enter your guess ({guessTracker + 1}): ");
+            }
+        }
+        else
+        {
+            if(userGuess == secretNumber){//success, good guess.
+                Console.WriteLine($"Good guess! It was {secretNumber} all along!");
+            }
+            else{//too many attempts.
+                Console.WriteLine("Unlucky! Play another game!");
+            }
+            break;//break and end regardless of outcome.
+        }
+    } while (trackedGuess != secretNumber && guessTracker <= 3);
 }
 
 GuessingGame();
