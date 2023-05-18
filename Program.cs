7a9ccdbd1af4ef005cdb40843443f06243bac621
Jason Harris <lord.jtharris@gmail.com>
2023-05-18 13:17:21 -0500

    Number is now random. Player is informed of attempts remaining.

--- Program.cs
+++ Program.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 
 void GuessingGame()
 {
+    Random random = new Random();
     int guessTracker = 0;
-    int secretNumber = 42;
+    int secretNumber = random.Next(1,100);
     int trackedGuess;
+    int maxAttempts = 3; // one fewer than the real number, because the guessTracker starts at 0
     Console.WriteLine("Welcome to the Guessing Game!");
     Console.WriteLine("You will be guessing my SECRET! number. The range is 1-100, inclusive.");
     Console.WriteLine("PLEASE: Only enter in a number!");
-    Console.WriteLine($"Enter your guess ({guessTracker + 1}): ");
-    
+    Console.WriteLine($"Enter your guess (Guess {guessTracker + 1} - {maxAttempts - guessTracker} Left!): ");
+
     do//do...while to keep track of the userGuess and compare it to an outside-scope variable, if the input can change.
     {
         int userGuess = int.Parse(Console.ReadLine());
         trackedGuess = userGuess;
 
-        if (userGuess != secretNumber && guessTracker != 3)
+        if (userGuess != secretNumber && guessTracker != maxAttempts)
         {
             Console.WriteLine("Alas, not this time, but maybe next time!");
-            if (guessTracker < 3)//prevents these lines from going through after the final input session.
+            if (guessTracker < maxAttempts)//prevents these lines from going through after the final input session.
             {
                 guessTracker++;
-                Console.WriteLine($"Enter your guess ({guessTracker + 1}): ");
+                Console.WriteLine($"Enter your guess (Guess {guessTracker + 1} - {maxAttempts - guessTracker} Left!): ");
             }
         }
         else
@@ -31,11 +33,11 @@ void GuessingGame()
                 Console.WriteLine($"Good guess! It was {secretNumber} all along!");
             }
             else{//too many attempts.
-                Console.WriteLine("Unlucky! Play another game!");
+                Console.WriteLine($"Unlucky! Play another game! The number was {secretNumber}!");
             }
             break;//break and end regardless of outcome.
         }
-    } while (trackedGuess != secretNumber && guessTracker <= 3);
+    } while (trackedGuess != secretNumber && guessTracker <= maxAttempts);
 }
 
 GuessingGame();
